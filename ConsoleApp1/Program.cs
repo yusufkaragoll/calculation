31fdd744b44f3771754215800fb5bd8d2febbc23
Yusuf 125758 <pzx103744@student.wsb.poznan.pl>
2020-12-22 21:30:36 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int vize, final, not, ortalama;
+
+            Console.WriteLine("vize");
+            vize = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("final");
+            final = Convert.ToInt32(Console.ReadLine());
+
+            ortalama = vize + final;
+            Console.WriteLine("Sonuc:" + ortalama);
+        }
+    }
+}
